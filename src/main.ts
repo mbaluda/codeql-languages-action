1e924bdafc9f75498214fbeb2c500e968a8bc813
Mauro Baluda <mbaluda@github.com>
2023-05-28 17:58:59 +0200

    Add logs

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,29 @@
 import * as core from '@actions/core'
-import { Octokit } from "@octokit/rest";
+import { Octokit } from "@octokit/rest"
 
 async function run(): Promise<void> {
   try {
     const owner = core.getInput('owner');
     const repo = core.getInput('repo');
-    const octokit = new Octokit();
+    core.info(`Repo: ${owner}/${repo}`);
+    core.info(`SHA: ${process.env.GITHUB_SHA}`);
 
-    const { data: tree} = await octokit.rest.git.getTree({
+    const { data: tree1 } = await new Octokit().rest.git.getTree({
       owner,
       repo,
       tree_sha: process.env.GITHUB_SHA ? process.env.GITHUB_SHA : "HEAD",
       recursive: "true",
     });
-  
+
+    const hasCPPFile = tree1.tree.some((item) => item.path?.match(/\.(cpp\|c\+\+|cxx|hpp|hh|h\+\+\|hxx|c|cc|h)$/));
+    core.info(`hasCPPFile: ${hasCPPFile}`);
+
+    const hasJSFile = tree1.tree.some((item) => item.path?.match(/\.\(js\|jsx\|mjs\|es\|es6\|htm\|html\|xhtm\|xhtms\|vue\|hbs\|ejs\|njk\|json\|yaml\|yml\|raml\|xml\|ts\|tsx\|mts\|cts\)$/));
+    core.info(`hasJSFile: ${hasJSFile}`);
+
     let languages = [];
-
-    const hasJSFile = tree.tree.some((item) => item.path?.match(/\.js$/));
-    if (hasJSFile) languages.push('javascript');
-
-    const hasCPPFile = tree.tree.some((item) => item.path?.match(/\.cpp$/));
     if (hasCPPFile) languages.push('cpp');
+    if (hasJSFile) languages.push('javascript');
 
     core.setOutput('languages', languages)
   } catch (error) {
